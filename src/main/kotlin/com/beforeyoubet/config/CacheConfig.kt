1167f4295cc4b342431913caa19969b97030c5b1
leandrodalbo <leandro.edbo@gmail.com>
2025-08-21 20:39:51 -0300

    adding redis cache

---
+++ src/main/kotlin/com/beforeyoubet/config/CacheConfig.kt
@@ -0,0 +1,40 @@
+package com.beforeyoubet.config
+
+import org.springframework.cache.CacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
+@Configuration
+class CacheConfig {
+
+    @Bean
+    fun cacheManager(redisConnectionFactory: LettuceConnectionFactory): CacheManager {
+
+        val defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .entryTtl(Duration.ofMinutes(10))
+
+
+        val cacheConfigs: Map<String, RedisCacheConfiguration> = mapOf(
+            "fetchMatches" to defaultConfig.entryTtl(Duration.ofMinutes(15)),
+            "fetchLeagueStandings" to defaultConfig.entryTtl(Duration.ofMinutes(30)),
+            "fetchFixtureOdds" to defaultConfig.entryTtl(Duration.ofMinutes(15)),
+            "fetchMatchDetails" to defaultConfig.entryTtl(Duration.ofMinutes(15)),
+            "fetchMatchEvents" to defaultConfig.entryTtl(Duration.ofMinutes(30)),
+            "fetchTeamDetails" to defaultConfig.entryTtl(Duration.ofMinutes(30)),
+            "fetchCoachDetails" to defaultConfig.entryTtl(Duration.ofMinutes(30)),
+            "fetchSquad" to defaultConfig.entryTtl(Duration.ofMinutes(30))
+        )
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(defaultConfig)
+            .withInitialCacheConfigurations(cacheConfigs)
+            .build()
+    }
+}
