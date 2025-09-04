030b90834d16bccb6d570dc3d5f0b5a15c7c2c9f
leandrodalbo <leandro.edbo@gmail.com>
2025-09-04 00:52:43 -0300

    world cup year property added

--- src/main/kotlin/match/insights/apidata/Apidata.kt
+++ src/main/kotlin/match/insights/apidata/Apidata.kt
@@ -63,26 +63,23 @@ class Apidata(
         jobs.mapValues { (_, job) -> job.await() }
     }
 
-    fun teamSquad(teamId: Int): Map<Int, List<PlayerResponse>> = runBlocking {
-        val page1: ApiPagingResponse<List<PlayerResponse>> =
-            apiSportsClient.fetchPlayers("/players?team=$teamId&season=${seasonProps.year}&page=1")
-        val totalPages = page1.paging.total;
-        val nextPage = 2;
+    fun teamSquad(teamId: Int): Map<Int, List<PlayerResponse>> =
+        apiSportsClient.fetchPlayers("/players?team=$teamId&season=${seasonProps.year}&page=1")
+            .let { res ->
+                if (res.response.isNotEmpty())
+                    mapOf(1 to res.response) + teamSquadPages(teamId, 2, res.paging.total, false)
+                else
+                    apiSportsClient.fetchPlayers("/players?team=$teamId&season=${seasonProps.worldCupYear}&page=1")
+                        .let { wcRes ->
+                            mapOf(1 to wcRes.response) + teamSquadPages(teamId, 2, wcRes.paging.total, true)
 
-        val jobs = (nextPage..totalPages).associateWith { page ->
-            async {
-                apiSportsClient.fetchPlayers(
-                    "/players?team=$teamId&season=${seasonProps.year}&page=$page"
-                ).response
-
+                        }
             }
-        }
 
-        mapOf(1 to page1.response) + jobs.mapValues { it.value.await() }
-    }
 
     fun leagueStandings(leagueId: Int): LeagueWithStandings? =
         apiSportsClient.fetchLeagueInfo("/standings?league=$leagueId&season=${seasonProps.year}")
+            ?: apiSportsClient.fetchLeagueInfo("/standings?league=$leagueId&season=${seasonProps.worldCupYear}")
 
     fun fetchAllOdds(fixtureId: Int): List<FixtureOdds> =
         apiSportsClient.fetchFixtureOdds("/odds?fixture=$fixtureId")
@@ -112,12 +109,43 @@ class Apidata(
 
     private fun lastFiveMatches(teamId: Int): List<MatchResponse> =
         apiSportsClient.fetchMatches("/fixtures?team=${teamId}&season=${seasonProps.year}")
-            .filter { it.fixture.status?.short == "FT" }
-            .sortDescendingByDate()
-            .take(5)
+            .let {
+                return if (it.isNotEmpty())
+                    it.filter { it.fixture.status?.short == "FT" }
+                        .sortDescendingByDate()
+                        .take(5)
+                else
+                    apiSportsClient.fetchMatches("/fixtures?team=${teamId}&season=${seasonProps.worldCupYear}")
+                        .filter { it.fixture.status?.short == "FT" }
+                        .sortDescendingByDate()
+                        .take(5)
+            }
 
     private fun teamOnLeagueMatches(teamId: Int, leagueId: Int): List<MatchResponse> =
         apiSportsClient.fetchMatches("/fixtures/?team=${teamId}&season=${seasonProps.year}&league=${leagueId}")
+            .let {
+                return it.ifEmpty { apiSportsClient.fetchMatches("/fixtures/?team=${teamId}&season=${seasonProps.worldCupYear}&league=${leagueId}") }
+            }
+
+
+    private fun teamSquadPages(
+        teamId: Int,
+        nextPage: Int,
+        totalPages: Int,
+        userWC: Boolean
+    ): Map<Int, List<PlayerResponse>> = runBlocking {
+
+        val jobs = (nextPage..totalPages).associateWith { page ->
+            async {
+                apiSportsClient.fetchPlayers(
+                    "/players?team=$teamId&season=${if (userWC) seasonProps.worldCupYear else seasonProps.year}&page=$page"
+                ).response
+
+            }
+        }
+
+        jobs.mapValues { it.value.await() }
+    }
 
 
     private fun List<MatchResponse>.sortDescendingByDate(): List<MatchResponse> {
--- src/main/kotlin/match/insights/props/SeasonProps.kt
+++ src/main/kotlin/match/insights/props/SeasonProps.kt
@@ -4,4 +4,4 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import java.time.LocalDate
 
 @ConfigurationProperties(prefix = "season")
-data class SeasonProps(val year: Int? = LocalDate.now().year)
+data class SeasonProps(val year: Int? = LocalDate.now().year, val worldCupYear: Int? = 2026)
